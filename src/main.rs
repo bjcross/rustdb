80eccee2805baa66a412ab653b122f2da3ce55df
Garrett Olson <garrettolson3@gmail.com>
2020-09-01 18:44:09 -0400

    Add serde and port_int

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate clap;
 use clap::App;
 use std::error::Error;
+use serde::{Serialize, Deserialize};
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
@@ -11,4 +12,6 @@ fn main() {
     println!("Create db present is {}", createdb);
     let port = matches.value_of("port").unwrap();
     println!("Port is {}", port);
+    let port_int = u32::from_str_radix(port, 10).unwrap();
+    println!("Port_int is {}", port_int);
 }
